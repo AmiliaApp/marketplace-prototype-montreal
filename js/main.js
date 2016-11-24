6c31f13d92a4f7eb87f2c29e552cd7f8aab90d3e
Martin Drapeau <martindrapeau@gmail.com>
2016-11-24 12:30:53 -0500

    first commit, ever!

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+  (function() {
+    var $form = $('#search');
+    var $select = $('#searchSelect');
+    var $jumbotronHome = $('#jumbotronHome');
+  
+    $select.on('change', function() {
+      var value = $select.val();
+
+      $jumbotronHome.css('backgroundImage', 'url(img/jumbotron-' + value + '.jpg)');
+
+      $form.attr('action', value + '-list.html');
+    });
+  })();
+
+});
